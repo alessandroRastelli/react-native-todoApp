17c58c2ff6080c70a92b80427fa571670c373624
alessandroRastelli <rastarm@hotmail.it>
2016-09-30 17:32:55 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -31,47 +31,51 @@ class InputTodo extends Component {
   render() {
     return (
       <View style={{padding: 10}}>
-	      	<View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-		      	<TextInput
-		          style={{flex:2,height: 40}}
-		          placeholder="Type here to insert Todo!"
-		          onChangeText={ this.onChangeValue.bind(this)}
-		        />
-		        <Button
-			        style={{flex:1,fontSize: 20, color: 'green'}}
-			        styleDisabled={{color: 'red'}}
-			        onPress={() => this.add()}>
-			        ADD
-		      	</Button>
-		    </View>
-	      	<View style={{flex: 1, flexDirection: 'row'}}>
-	      		<Text style={{flex: 1}}>ToDo:{this.state.todos.filter((todo) => {
-									      return ( ( todo.check == false));
-									    	}).length } </Text>
-				<Text style={{flex: 1}}>Done:
-				{this.state.todos.filter((todo) => {
-			    							return ( ( todo.check == true));
-									    	}).length} </Text>
-	        
-	     	</View>
+      	<View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+	      	<TextInput
+	          style={{flex:2,height: 40}}
+	          placeholder="Type here to insert Todo!"
+	          onChangeText={ this.onChangeValue.bind(this)}
+	        />
+	        <Button
+		        style={{flex:1,fontSize: 20, color: 'green'}}
+		        styleDisabled={{color: 'red'}}
+		        onPress={() => this.add()}>
+		        ADD TODO
+	      	</Button>
+	    	</View>
+      	<View style={{flex: 1, flexDirection: 'row'}}>
+      		<Text style={ {flex: 1} }> ToDo: 
+      			{this.state.todos.filter((todo) => { return ( 
+      				( todo.check == false));
+      				}).length 
+      			} 
+      		</Text>
+					<Text style={{flex: 1}}>Done:
+						{this.state.todos.filter((todo) => { return (
+						 	( todo.check == true));
+							}).length
+						}
+					</Text>
+     		</View>
        
-      		{this.state.todos.map((todo,i)=>
+      	{this.state.todos.map((todo,i)=>
       		<View style={{flex: 1, padding: 4, flexDirection: 'row', justifyContent: 'space-between'}}>
 		      	<CheckBox
 		      	  label={todo.name}
-				  checked={todo.check}
-				  onChange={() => this.checkTodo(i)}
-				/>
-		         <Button
+						  checked={todo.check}
+						  onChange={() => this.checkTodo(i)}
+						/>
+		        <Button
 			        style={{flex:1, color: 'green'}}
 			        styleDisabled={{color: 'red'}}
 			        onPress={() => this.remove(i)}>
 			        Delete
 	      		</Button>
-            </View>
+          </View>
          )}
       </View>
-         
+
     );
   }
 }
